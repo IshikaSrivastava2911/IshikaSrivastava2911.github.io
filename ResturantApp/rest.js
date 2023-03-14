75b95915c8e246420279915e7dec31a5e0a5f111
Ishika Srivastava <ishika.srivastava@zemosolabs.com>
2023-03-14 11:24:10 +0530

    Resolved Problems in popup

--- ResturantApp/rest.js
+++ ResturantApp/rest.js
@@ -1,37 +1,12 @@
 //table array to store table objects
-
 const tables=[
-    {t_id:1, amount: 0.00, itemList:[]},
-    {t_id:2, amount: 0.00, itemList:[]},
-    {t_id:3, amount: 0.00, itemList:[]},
-    {t_id:4, amount: 0.00, itemList:[]},
-    {t_id:5, amount: 0.00, itemList:[]},
+    {t_id:1, amount: 0.00, itemList:{}, itemCount:0},
+    {t_id:2, amount: 0.00, itemList:{}, itemCount:0},
+    {t_id:3, amount: 0.00, itemList:{}, itemCount:0},
+    {t_id:4, amount: 0.00, itemList:{}, itemCount:0},
+    {t_id:5, amount: 0.00, itemList:{}, itemCount:0},
 ];
 
-//menus array for all the menu items
-
-const menus=[
-    {menu_item:"French Fries",price: 120, type: "Starter"},
-    {menu_item:"Paneer butter Masala",price: 250, type: "Main Course"},
-    {menu_item:"Mojito",price: 150, type: "Beverages"},
-    {menu_item:"Cheese Cake",price: 200, type: "Desserts"},
-    {menu_item:"Mac-n-cheese",price: 200, type: "Starter"},
-    {menu_item:"Pizza",price: 300, type: "Starter"},
-    {menu_item:"Cake",price: 100, type: "Desserts"},
-    {menu_item:"Chilli Paneer",price: 240, type: "Main Course"},
-    {menu_item:"Orange juice",price: 80, type: "Beverages"},
-    {menu_item:"Gulab Jamun",price: 50, type: "Desserts"},
-    {menu_item:"Manchurian",price: 180, type: "Main Course"},
-    {menu_item:"Maggie",price: 70, type: "Starter"},
-    {menu_item:"Burger",price: 70, type: "Starter"},
-
-];
-
-//loading the table and menu when the webpage is loaded
-// window.onload =()=>{
-//     tableDisplay();
-//     menuDisplay();
-// }
 
 //function to display tables
 const tableDisplay=()=>{
@@ -89,7 +64,7 @@ const tableDisplay=()=>{
         tblTotalItem.className ="total";
         tblTotalItem.id=`TotalItems${i+1}`;
 
-        tblTotalItem.innerHTML = `Total Items: ${tables[i].itemList.length}`;
+        tblTotalItem.innerHTML = `Total Items: ${tables[i].itemCount}`;
 
         pt.appendChild(priceItem);
         pt.appendChild(tblTotalItem);
@@ -100,6 +75,25 @@ const tableDisplay=()=>{
     }
 };
 
+
+//menus array for all the menu items
+
+const menus=[
+    {menu_item:"French Fries",price: 120, type: "Starter"},
+    {menu_item:"Paneer butter Masala",price: 250, type: "Main Course"},
+    {menu_item:"Mojito",price: 150, type: "Beverages"},
+    {menu_item:"Cheese Cake",price: 200, type: "Desserts"},
+    {menu_item:"Mac-n-cheese",price: 200, type: "Starter"},
+    {menu_item:"Pizza",price: 300, type: "Starter"},
+    {menu_item:"Cake",price: 100, type: "Desserts"},
+    {menu_item:"Chilli Paneer",price: 240, type: "Main Course"},
+    {menu_item:"Orange juice",price: 80, type: "Beverages"},
+    {menu_item:"Gulab Jamun",price: 50, type: "Desserts"},
+    {menu_item:"Manchurian",price: 180, type: "Main Course"},
+    {menu_item:"Maggie",price: 70, type: "Starter"},
+    {menu_item:"Burger",price: 70, type: "Starter"},
+
+];
 
 //function for displaying Menu
 const menuDisplay=()=>{
@@ -158,8 +152,6 @@ const menuDisplay=()=>{
     }
 
 };
-
-
 
 
 
@@ -227,62 +219,6 @@ const menuSearch=()=>{
 }
 
 
-
-// //For DRAG AND DROP
-
-// //select all the menu items and add dragstart event listener to them
-
-// const menuItems = document.querySelectorAll('.item');
-
-// menuItems.forEach(item =>{
-//     item.addEventListener('dragstart',event =>{
-//         event.dataTransfer.setData('itemName',item.dataset.menu_item);
-//         event.dataTransfer.setData('itemPrice',item.dataset.price);
-//     });
-
-// });
-
-
-// //Select all the table elemets and add dragover and drag drop event listener
-
-// const tableItem = document.querySelectorAll('.eachtable');
-
-// tableItem.forEach(table =>{
-//     table.addEventListener('dragover', event =>{
-//         event.preventDefault();
-//         event.dataTransfer.dropEffect ='move';
-//         table.classList.add('active');
-//     });
-
-//     table.addEventListener('drop', event=>{
-//         event.preventDefault();
-//         const itemName =event.dataTransfer.getData('itemName');
-//         const itemPrice = parseFloat(event.dataTransfer.getData('itemPrice'));
-//         const tableNumber = table.dataset.t_id;
-//         // Update table order details
-//         const itemCountElement = table.querySelector('.total');
-//         const totalCostElement = table.querySelector('.price');
-//         let itemCount = parseInt(itemCountElement.textContent);
-//         let totalCost = parseFloat(totalCostElement.textContent);
-//         itemCount++;
-//         totalCost += itemPrice;
-
-
-
-
-//         itemCountElement.textContent = itemCount;
-//         totalCostElement.textContent = totalCost.toFixed(2);
-
-
-//         table.classList.remove('active');
-//     });
-// });
-
-// // let activeTable = null;
-
-
-
-
 //drag function
 const drag =(e)=>{
     // console.log("called drag");
@@ -299,103 +235,63 @@ const drop =(e)=>{
     //get id
     let arr = data.split("");
     let item_id =arr[4];
-// table1
-    //table name
+
+
     let tableid = e.target.id;
-    // console.log(tableid);
+   
     
-    // console.log("itemid: "+item_id);
-    // calling upateItemList funtion
+    // calling funtion to update the items on the table
     addMenuItemOnTable(tableid.slice(5),menus[item_id-1].menu_item,menus[item_id-1].price)
-    // console.log("tablesliceid"+tableid.slice(5));
-    // calling updateTable function
-    itemAddedinTable(tableid.slice(5));
 
-
+    updateTableUI(tableid.slice(5));
+    
 };
 
 
 //menu item on table updation
 const addMenuItemOnTable =(t_id, name, price)=>{
-
-    // console.log(t_id + name + price);
     let itemExists =false;
-
     //check if menu-item already exists
-    for( let i=0; i< tables[t_id-1].itemList.length; i++){
-        if (tables[t_id-1].itemList[i].name ==name){
-            itemExists=true;
-        }
+    if (`${name}` in tables[t_id-1].itemList){
+        itemExists=true;
     }
-    // console.log("MenuItem");
     //we have to increase count if item is already present
     if(itemExists){
-        for (let i=0; i<tables[t_id-1].itemList.length; i++){
-            if(tables[t_id-1].itemList[i].name==name){
-                tables[t_id-1].itemList[i].count++;
-                break;
-            }
-        }
+        tables[t_id-1].itemList[`${name}`].count +=1;
+
     }//else enter item in itemList array in table
     else{
         let itemObject ={
-            name: name,
-            price: price,
-            count: 1,
-        }
-        tables[t_id-1].itemList.push(itemObject);
-        // console.log(itemObject);
-        // console.log("tble item"+itemList);
-        // console.log("this is item "+ itemList[0]);
+            "name": name,
+            "price": price,
+            "count": 1,
+        };
+    
+        tables[t_id-1].itemList[`${name}`]=itemObject;
+        console.log(tables[t_id-1].itemList);
     }
+    //update the total count and total amount
+    tables[t_id-1].itemCount +=1;
+    tables[t_id-1].amount += price;
+
+    console.log(tables[t_id-1].itemCount); 
+    console.log(tables[t_id-1].amount);
+
+    
 };
 
-
-//update the total amount and total number of items on each table
-const itemAddedinTable =(t_id) =>{
-
-    let itemAmount = 0;
-    let itemCount =0;
-
-    //using itemList array, get the total amount and count of item from the array
-    for (let i=0; i<tables[t_id-1].itemList.length; i++){
-
-        itemAmount+=(tables[t_id-1].itemList[i].price*tables[t_id-1].itemList[i].count)
-
-        itemCount += (tables[t_id-1].itemList[i].count);
-        
-    }
-
+const updateTableUI = (t_id)=>{
+    //Updating the price on the UI
     let totalPrice = document.querySelector(`#priceT${t_id}`);
     
     let totaltblItems = document.querySelector(`#TotalItems${t_id}`);
 
     // let ptags= document.getElementById(`pTag${i+1}`)
-    totalPrice.innerHTML = `Rs. ${itemAmount} |`;
-    totaltblItems.innerHTML = `Total Items: ${itemCount}`;
-
-    // console.log(totalPrice);
-    // console.log(totaltblItems);
+    totalPrice.innerHTML = `Rs. ${tables[t_id-1].amount} |`;
+    totaltblItems.innerHTML = `Total Items: ${tables[t_id-1].itemCount}`;
+}
 
 
-
-    // console.log("Print if table updates")
-    // console.log("UT" +itemCount);
-    // console.log("UT"+ itemAmount);
-    // console.log("updateTable");
-
-
-
-};
-
-// const itemRemove = (_id,index ) => {
-//         tables[_id-1].itemList[index] = {
-//             name : "",
-//             cost : 0,
-//             count : 0,
-//         };
-//         Billprinting(_id);
-// }
 
 
 
@@ -416,71 +312,239 @@ const openBill=(e)=>{
     //call
     let updtID= e.target.id;
     // console.log(updtID);
-    Billprinting(updtID.slice(5));
+    popupBill(updtID.slice(5));
+    updatePopTotalAmt(updtID.slice(5));
 }
 
 //function to close modal
-function closeBill(){
+const closeBill=(e)=>{
+    // console.log("close");
     modal.style.display='none';
+    let updtID= e.target.id;
+    // deletePopTab(updtID);
 }
 
 //function to close modal if outside click
 const clickOutside=(e)=>{
+    // console.log("closeout");
     if(e.target== modal){
         modal.style.display='none';
     }
+    let updtID= e.target.id;
+    console.log(updtID);
+    // deletePopTab(updtID);
 }
 
 //Listen for close click
-closeBtn.addEventListener('click',closeBill);
+closeBtn.addEventListener('click',(event)=>{closeBill(event)});
 //Listen for outside click
-window.addEventListener('click', clickOutside);
+window.addEventListener('click', (event)=>{clickOutside(event)});
 
 
 
 
 
 // // this function is used to update the bill
-const Billprinting =(t_id) => {
+const popupBill =(t_id) => {
     // console.log("bill printing");
+    const tableNameonHeader = document.getElementById('tableHeaderName');
+    tableNameonHeader.innerText = `| Table-${t_id}`;
 
     let itemNum=1;
+    const keys = Object.keys(tables[t_id-1].itemList);
+    // console.log("Keys: "+keys);
+    const tableHeader = document.getElementById('popTable');
+    tableHeader.innerHTML = `
+                    <tr >
+                            <th>S No.</th>
+                            <th>Item</th>
+                            <th>Price</th>
+                            <th>Quantity</th>
+                            <th>Delete</th>
+                    </tr>
+    `;
+
     
-    for(let i=0;i<tables[t_id-1].itemList.length;i++){
+    for(let i=0;i<keys.length;i++){
+
+        let countPop =tables[t_id-1].itemList[keys[i]].count;
+        let namePop =tables[t_id-1].itemList[keys[i]].name;
+        let pricePop = tables[t_id-1].itemList[keys[i]].price;
+
+        
+        // console.log("counting: "+ countPop)
     
-        if(tables[t_id-1].itemList[i].count!=0){
-
-            document.getElementById('popTable').innerHTML += `<tr>
-            <td>${itemNum++}</td>
-
-            <td>${tables[t_id-1].itemList[i].name}</td>
-
-            <td>${tables[t_id-1].itemList[i].price}</td>
-
+        if(countPop!=0){
+            const tr1 = document.createElement('tr');
+            tr1.id=`row-${itemNum}`;
             
-            </tr>`;
+            const td1 = document.createElement('td');
+            td1.innerHTML = `${itemNum++}`;
+            tr1.appendChild(td1);
+
+            const td2 = document.createElement('td');
+            td2.innerHTML = `${namePop}`;
+            tr1.appendChild(td2);
+
+            const td3 = document.createElement('td');
+            td3.innerHTML=`${pricePop}`;
+            tr1.appendChild(td3);
+
+            const td4 = document.createElement('td');   //table description         
+            const ipt = document.createElement('input');
+            ipt.setAttribute("data-item",namePop);
+            ipt.setAttribute("type","number");
+            ipt.setAttribute("min",1);
+            ipt.setAttribute("max",6);
+            // ipt.min=1;
+            // ipt.max=6;
+            ipt.setAttribute("id","popup-item-change");
+            // console.log(t_id);
+            //  console.log(namePop);
+
+            ipt.addEventListener('change', (event)=>{
+                change(event,t_id,namePop);
+            });
+            td4.appendChild(ipt);
+            tr1.appendChild(td4); //appended in table row
+
+            const td5 = document.createElement('td');
+            td5.innerHTML=`<button class="btn" id="btn"><i class="fa fa-trash"></i></button>`;
+            tr1.appendChild(td5);
+
+            document.getElementById('popTable').appendChild(tr1);
+
+            const firstchild = td5.parentNode.firstChild.innerHTML;
+            console.log("first:"+ firstchild);
+
             let tBillPrice = document.querySelector(`#priceT${t_id}`).innerHTML; 
 
             document.getElementById('grandTotal').innerHTML= `Total Bill: ${tBillPrice}`;
+
+            document.getElementById("btn").addEventListener('click',(event)=>{
+                itemRemove(event,t_id,`row-${firstchild}`, namePop);
+            })
         
             // <td><button class="itemRemove" onclick="itemRemove(${t_id},${i})">&times;</button></td>
         }
         
     }
+
+    // itemAddedinTable(t_id);
+    const billBtn = document.getElementById('Bill-btn');
+        billBtn.addEventListener('click', (event)=>{
+            generateBill(event,t_id);
+        });
+
     
 }
 
+const updatePopTotalAmt=(t_id)=>{
+    let tBillPrice = document.querySelector(`#priceT${t_id}`).innerHTML; 
 
-const billBtn = document.getElementById('Bill-btn');
-billBtn.addEventListener('click', (event)=>{
-    generateBill(event);
-});
-
-const generateBill=(e)=>{
-    const grndtotal= document.getElementById('grandTotal').innerHTML;
-    alert("Your Total Bill is: "+ grndtotal);
+    document.getElementById('grandTotal').innerHTML= `Total Bill: ${tBillPrice}`;
 }
 
+
+const change=(e,t_id,namePop)=>{
+//actual amount count
+// 2
+// itemList={
+//     "french fries": { "name":"fench fries", "count": 2, "price": 254}
+// }
+// table[t_id-1].itemList[namePop].count
+
+    const inputValue = parseInt(e.target.value);
+    const itemPrice = tables[t_id-1].itemList[namePop].price;
+    const newtotalAmount = inputValue*itemPrice;
+    // console.log(newtotalAmount);
+    // console.log("Price: "+itemPrice);
+    const initialCount = tables[t_id-1].itemList[namePop].count;
+
+    //removing previous value
+    tables[t_id-1].itemCount -= initialCount;
+    tables[t_id-1].amount -= initialCount*itemPrice;
+
+    // setting current value
+    tables[t_id-1].itemList[namePop].count = inputValue;
+    tables[t_id-1].itemCount += inputValue;
+    tables[t_id-1].amount += inputValue*itemPrice;
+
+    console.log(tables[t_id-1].itemCount);
+    console.log(tables[t_id-1].amount);
+
+    //update UI
+    // updateTableUI(t_id);
+    let totalPrice = document.querySelector(`#priceT${t_id}`);
+    
+    let totaltblItems = document.querySelector(`#TotalItems${t_id}`);
+
+    totalPrice.innerHTML = `Rs. ${tables[t_id-1].amount} |`;
+    totaltblItems.innerHTML = `Total Items: ${tables[t_id-1].itemCount}`;
+
+    
+}
+
+const itemRemove = (e,t_id,r_id,menuItem) => {
+     
+    console.log(r_id);
+    tables[t_id-1].amount -=tables[t_id-1].itemList[menuItem].price*tables[t_id-1].itemList[menuItem].count;
+    tables[t_id-1].itemCount -=tables[t_id-1].itemList[menuItem].count;
+    
+    //after removing the price and amount from total, remove the item from that table's object
+    tables[t_id-1].itemList[menuItem].name="";
+    tables[t_id-1].itemList[menuItem].count=0;
+    tables[t_id-1].itemList[menuItem].price=0;
+    // tables[t_id-1].itemList[menuItem].remove();
+
+    // console.log("menu: "+ tables[t_id-1].itemList[menuItem] )
+
+    // const table = document.getElementById('popTable');
+    //empty the row UI
+    const row= document.getElementById(r_id);
+    row.innerHTML = "";
+
+    // //here if I call popupBill then new bill should displayes?
+    popupBill(t_id);
+    
+    //to update the Table UI
+    let totalPrice = document.querySelector(`#priceT${t_id}`);
+    
+    let totaltblItems = document.querySelector(`#TotalItems${t_id}`);
+
+    totalPrice.innerHTML = `Rs. ${tables[t_id-1].amount} |`;
+    totaltblItems.innerHTML = `Total Items: ${tables[t_id-1].itemCount}`;
+
+    // popupBill(t_id);
+    updateTableUI(t_id);
+            
+}
+
+
+
+// const deletePopTab =(t_id)=>{
+    
+//     const popUpTableRow = document.getElementById(`row${t_id}`);
+//     popUpTableRow.remove();
+
+//     // popupBill(t_id);
+
+// }
+
+
+
+
+const generateBill=(e, t_id)=>{
+    const grndtotal= document.getElementById('grandTotal').innerHTML;
+    alert("Your Total Bill is: "+ grndtotal);
+
+    // setting the itemList of that table with free array
+    tables[t_id-1].itemList={};
+    document.getElementById(`priceT${t_id}`).innerHTML=`Rs. 0 |`;
+    document.getElementById(`TotalItems${t_id}`).innerHTML=`Total Items: 0`;
+    // calling updateBill function
+    // popupBill(t_id);
+}
 
 
 
